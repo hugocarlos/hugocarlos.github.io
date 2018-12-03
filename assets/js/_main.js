633978ee3d7711ccf3719feed70c649d26ceb355
Hugo Carlos Samano Sanchez <hugo.samano@embl.de>
2018-12-03 17:14:52 +0100

    removing HEAD master

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-
-
-
-
-
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -1374,4 +1367,3 @@ $(document).ready(function() {
     midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
   });
 });
->>>>>>> origin/master
